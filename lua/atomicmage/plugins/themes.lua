d8f2c73431794e7895e70b53256c6094d806408a
Prajwal Shrestha <pjmessi25@icloud.com>
2024-03-30 20:33:18 +0545

    Remove transparent background

--- lua/atomicmage/plugins/themes.lua
+++ lua/atomicmage/plugins/themes.lua
@@ -14,8 +14,8 @@ M.setup = function(opts)
       vim.cmd.colorscheme 'rose-pine'
 
       -- Transparent background (The Primegan - atomicmage)
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
